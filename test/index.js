ef41792c846b3ed608fe90647c69edd58a06a541
Florent Kolosowski <fkolosowski@ivsweb.com>
2021-10-27 14:28:24 +0200

    better test?

--- test/index.js
+++ test/index.js
@@ -12,17 +12,21 @@ describe('Testing single-instance-process', () => {
     expect(isServer).to.be(true);
   });
 
-  it('should fail to create or connect to server', async () => {
-    try {
-      await sip('');
-    } catch(err) {
-      expect(err.code).to.eql('EACCES');
-    }
-  });
-
   it('should connect to sip and be a client', async () => {
     let isServer = await sip(PIPE_KEY);
 
     expect(isServer).to.be(false);
+  });
+
+  it('should fail to create or connect to server', async () => {
+    let catched = '';
+
+    try {
+      await sip('');
+    } catch(err) {
+      catched = err.code;
+    } finally {
+      expect(catched).to.not.be.empty();
+    }
   });
 });
